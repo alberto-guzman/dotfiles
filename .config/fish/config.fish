ff94d40414d7542c4e6c58284c75fcbcb1480e34
Alberto Guzman-Alvarez <alvarez1991@gmail.com>
2023-01-23 15:45:54 -0800

    Delete .config/fish directory

--- .config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
